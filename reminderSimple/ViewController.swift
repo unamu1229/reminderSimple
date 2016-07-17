4bccd5702f35e0a2bd2e6ea8ac85d85b5a29f310
米田宏 <yonedahiroshi1229@gmail.com>
2016-07-17 18:38:35 +0900

    XcodeのSingleViewApplicationを作成した。

---
+++ reminderSimple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reminderSimple
+//
+//  Created by 米田宏 on 2016/07/17.
+//  Copyright © 2016年 hiroshiyoneda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
