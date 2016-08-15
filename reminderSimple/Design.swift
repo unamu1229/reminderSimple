2ac6c72e5573e4668fd250f87641184d86413738
米田宏 <yonedahiroshi1229@gmail.com>
2016-08-15 15:50:52 +0900

    ボタンのデザインを角丸にした

---
+++ reminderSimple/Design.swift
@@ -0,0 +1,30 @@
+//
+//  Design.swift
+//  reminderSimple
+//
+//  Created by 米田宏 on 2016/08/15.
+//  Copyright © 2016年 hiroshiyoneda. All rights reserved.
+//
+import UIKit
+
+@IBDesignable class CustomButton: UIButton {
+    
+    // 角丸の半径(0で四角形)
+    @IBInspectable var cornerRadius: CGFloat = 0.0
+    
+    // 枠
+    @IBInspectable var borderColor: UIColor = UIColor.clearColor()
+    @IBInspectable var borderWidth: CGFloat = 0.0
+    
+    override func drawRect(rect: CGRect) {
+        // 角丸
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = (cornerRadius > 0)
+        
+        // 枠線
+        self.layer.borderColor = borderColor.CGColor
+        self.layer.borderWidth = borderWidth
+        
+        super.drawRect(rect)
+    }
+}
